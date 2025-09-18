6fdc24981da0e5cf11eface59520b63bcc3f0971
Rangga Andhika Fadillah <50351226+randhika21@users.noreply.github.com>
2025-09-18 18:54:18 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const admin = require('firebase-admin');
 // Ambil kredensial dari environment variable
 const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
 
+// Fix pem key by replacing escaped newlines with real newlines
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+
 // Inisialisasi Firebase Admin
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
